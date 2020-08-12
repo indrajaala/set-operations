9e7217a333ca3d9a96e75edc3ff128089c6065cf
indrajaala <indrajaala@gmail.com>
2020-08-12 20:27:33 +0530

    add cjs budle in dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,20 @@
+import pkg from "./package.json";
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
     input: 'src/index.ts',
-    output: [{
-        file: "dist/index.js",
-        format: 'umd',
-        name: "setOperations"
-    },
+    output: [
         {
-            file: 'dist/index.esm.js',
+            file: pkg.browser,
+            format: 'umd',
+            name: "setOperations"
+        },
+        {
+            file: pkg.main,
+            format: "cjs",
+        },
+        {
+            file: pkg.module,
             format: 'es',
         }
     ],
