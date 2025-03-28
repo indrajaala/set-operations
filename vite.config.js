2f38c2f25b60481cf3b1f2704c34018dd72e9ce9
indrajaala <indrajaala@gmail.com>
2025-03-28 18:25:46 +0530

    #v3: Init Commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import {dirname, resolve} from "node:path";
+import {fileURLToPath} from "node:url";
+import {defineConfig} from "vite";
+import dts from "vite-plugin-dts";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     build: {
         lib: {
-            // Could also be a dictionary or array of multiple entry points
-            entry: resolve(__dirname, 'src/main.ts'),
-            name: 'main',
-            // the proper extensions will be added
-            fileName: 'main'
-        },
+            entry: resolve(__dirname, "src/main.ts"),
+            name: "main",
+            fileName: "main"
+        }
     },
-    plugins: [dts()]
-})
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src")
+        }
+    },
+    plugins: [dts({rollupTypes: true})]
+});
